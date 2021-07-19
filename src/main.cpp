16f3a34a5000092cbcdc601655fd1f1a507cc9e1
navono <navono007@gmail.com>
2021-07-19 20:46:03 +0800

    refactor: use nullptr

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,8 @@ int main() {
 
   // glfw window creation
   // --------------------
-  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-  if (window == NULL) {
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
