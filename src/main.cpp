0e2741cbf908bc53eeb07983a70f0bc347a43a36
navono <navono007@gmail.com>
2021-07-25 14:33:38 +0800

    feat: 创建一个三角形

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,21 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+const char* vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char* fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 int main() {
   auto logger = initLogger();
   LOG_INFO(logger, "Hello, World!");
@@ -34,7 +49,7 @@ int main() {
   // --------------------
   GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
   if (window == nullptr) {
-    std::cout << "Failed to create GLFW window" << std::endl;
+    LOG_ERROR(logger, "Failed to create GLFW window");
     glfwTerminate();
     return -1;
   }
@@ -44,9 +59,104 @@ int main() {
   // glad: load all OpenGL function pointers
   // ---------------------------------------
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
+    LOG_ERROR(logger, "Failed to initialize GLAD");
     return -1;
   }
+
+  // 使用着色器语言GLSL(OpenGL Shading Language)编写着色器
+  // 创建一个顶点（Vertex Shader）
+  // build and compile our shader program
+  // ------------------------------------
+  // vertex shader
+  auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  // 着色器与源码绑定
+  glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+  // 编译
+  glCompileShader(vertexShader);
+
+  // 编译错误检测
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+    LOG_ERROR(logger, "ERROR::SHADER::VERTEX::COMPILATION_FAILED. {}", infoLog);
+    return -1;
+  }
+
+  // 创建片段着色器（计算像素最后的颜色输出
+  auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+  glCompileShader(fragmentShader);
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+    LOG_ERROR(logger, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED. {}", infoLog);
+    return -1;
+  }
+
+  // link shaders
+  // 着色器程序对象(Shader Program Object)
+  // 多个着色器合并之后并最终链接完成的版本。如果要使用刚才编译的着色器我们必须把它们链接(Link)为一个着色器程序对象，
+  // 然后在渲染对象的时候激活这个着色器程序。已激活着色器程序的着色器将在我们发送渲染调用的时候被使用。
+  // 当链接着色器至一个程序的时候，它会把每个着色器的输出链接到下个着色器的输入。当输出和输入不匹配的时候，
+  // 会得到一个连接错误。
+  auto shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+    LOG_ERROR(logger, "ERROR::SHADER::PROGRAM::PROGRAMMING_FAILED. {}", infoLog);
+    return -1;
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  // 标准化设备坐标
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f,  // left
+      0.5f,  -0.5f, 0.0f,  // right
+      0.0f,  0.5f,  0.0f   // top
+  };
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
+  glBindVertexArray(VAO);
+
+  // 使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  // 把之前定义的顶点数据复制到缓冲的内存中
+  // 显卡如何管理给定的数据：
+  // GL_STATIC_DRAW ：数据不会或几乎不会改变。
+  // GL_DYNAMIC_DRAW：数据会被改变很多。
+  // GL_STREAM_DRAW ：数据每次绘制时都会改变。
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex
+  // buffer object so afterwards we can safely unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens.
+  // Modifying other VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when
+  // it's not directly necessary.
+  glBindVertexArray(0);
+
+  // uncomment this call to draw in wireframe polygons.
+  //  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   // render loop
   // -----------
@@ -60,11 +170,24 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // draw our first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no need to bind it every time, but we'll do
+                             // so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(0); // no need to unbind it every time
+
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
     // -------------------------------------------------------------------------------
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
 
   // glfw: terminate, clearing all previously allocated GLFW resources.
   // ------------------------------------------------------------------
