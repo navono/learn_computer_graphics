8c3754cb967d761081b04effe0759d633173cad6
navono <navono007@gmail.com>
2021-07-26 17:07:26 +0800

    docs: 增加备注

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// layout (location=0) in vec3 aPos; // 位置变量的属性位置值为0
 const char* vertexShaderSource =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -22,6 +23,8 @@ const char* vertexShaderSource =
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
+
+// out vec4 FragColor; // 为片段着色器指定一个颜色输出
 const char* fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
@@ -129,8 +132,8 @@ int main() {
   unsigned int VBO, VAO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
   glBindVertexArray(VAO);
 
   // 使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)
@@ -143,7 +146,18 @@ int main() {
   // GL_STREAM_DRAW ：数据每次绘制时都会改变。
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  // 设置顶点属性指针
+  // 第一个参数指定我们要配置的顶点属性。我们在顶点着色器中使用layout(location =
+  // 0)定义了position顶点属性的位置值(Location)，它把顶点属性的位置值设置为0。因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
+  // 第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
+  // 第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
+  // 第四个参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间。我们把它设置为GL_FALSE。
+  // 第五个参数叫做步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。由于下个组位置数据在3个float之后，我们把步长设置为3
+  // * sizeof(float)。要注意的是由于我们知道这个数组是紧密排列的（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。
+  // 最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+  // 以顶点属性位置值作为参数，启用顶点属性；顶点属性默认是禁用的。
   glEnableVertexAttribArray(0);
 
   // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex
@@ -172,8 +186,14 @@ int main() {
 
     // draw our first triangle
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no need to bind it every time, but we'll do
-                             // so to keep things a bit more organized
+
+    // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit
+    // more organized
+    glBindVertexArray(VAO);
+
+    // 第一个参数是我们打算绘制的OpenGL图元的类型
+    // 第二个参数指定了顶点数组的起始索引，我们这里填0
+    // 最后一个参数指定我们打算绘制多少个顶点
     glDrawArrays(GL_TRIANGLES, 0, 3);
     // glBindVertexArray(0); // no need to unbind it every time
 
