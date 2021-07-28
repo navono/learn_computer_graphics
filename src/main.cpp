bc985d75a3b421658fe9274cdbbe5729969b2117
navono <navono007@gmail.com>
2021-07-28 08:59:21 +0800

    feat: texture

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
+#include <filesystem>
 
 #include "logger.h"
 #include "shader.h"
+#include "stb_image.h"
 
 // clang-format on
 
@@ -15,23 +16,6 @@ void processInput(GLFWwindow* window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-const char* vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "}\0";
-
-const char* fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = ourColor;\n"
-    "}\n\0";
 
 int main() {
   auto logger = initLogger();
@@ -66,67 +50,106 @@ int main() {
     return -1;
   }
 
-  Shader shader("./shaders/3.3.shaders.vs", "./shaders/3.3.shaders.fs", logger);
+  Shader shader("./resources/shaders/4.2.texture.vs", "./resources/shaders/4.2.texture.fs", logger);
+
+  auto binPath = std::filesystem::current_path();
 
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
-  // 标准化设备坐标
   GLfloat vertices[] = {
-      // 位置              // 颜色
-      0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // 右下
-      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // 左下
-      0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f   // 顶部
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+  };
+  GLuint indices[] = {
+      0, 1, 3,  // first triangle
+      1, 2, 3   // second triangle
   };
 
-  GLuint VBO, VAO;
+  GLuint VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
   glBindVertexArray(VAO);
 
-  // 使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-  // 把之前定义的顶点数据复制到缓冲的内存中
-  // 显卡如何管理给定的数据：
-  // GL_STATIC_DRAW ：数据不会或几乎不会改变。
-  // GL_DYNAMIC_DRAW：数据会被改变很多。
-  // GL_STREAM_DRAW ：数据每次绘制时都会改变。
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  // 设置顶点属性指针
-  // 第一个参数指定我们要配置的顶点属性。我们在顶点着色器中使用layout(location =
-  // 0)定义了position顶点属性的位置值(Location)，它把顶点属性的位置值设置为0。因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
-  // 第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
-  // 第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
-  // 第四个参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间。我们把它设置为GL_FALSE。
-  // 第五个参数叫做步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。由于下个组位置数据在3个float之后，我们把步长设置为3
-  // * sizeof(float)。要注意的是由于我们知道这个数组是紧密排列的（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。
-  // 最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  // 位置属性
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  // 颜色属性
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex
-  // buffer object so afterwards we can safely unbind
-  //  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  // load and create a texture
+  // -------------------------
+  GLuint texture1, texture2;
+  // texture 1
+  // ---------
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                  GL_REPEAT);  // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens.
-  // Modifying other VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when
-  // it's not directly necessary.
-  //  glBindVertexArray(0);
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true);  // tell stb_image.h to flip loaded texture's on the y-axis.
+  auto containerImgPath = "./resources/textures/container.jpg";
+  unsigned char* data = stbi_load(containerImgPath, &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    LOG_ERROR(logger, "Failed to load texture: {}", containerImgPath);
+  }
+  stbi_image_free(data);
 
-  glBindVertexArray(VAO);
+  // texture 2
+  // ---------
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                  GL_REPEAT);  // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  auto awesomefaceImgPath = "./resources/textures/awesomeface.png";
+  data = stbi_load(awesomefaceImgPath, &width, &height, &nrChannels, 0);
+  if (data) {
+    // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data
+    // type is of GL_RGBA
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    LOG_ERROR(logger, "Failed to load texture: {}", awesomefaceImgPath);
+  }
+  stbi_image_free(data);
 
-  // uncomment this call to draw in wireframe polygons.
-  // 第一个参数表示我们打算将其应用到所有的三角形的正面和背面
-  // 第二个参数告诉我们用线来绘制
-  //  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+  // -------------------------------------------------------------------------------------------
+  shader.use();  // don't forget to activate/use the shader before setting uniforms!
+  // either set it manually like so:
+  glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
+  // or set it via the texture class
+  shader.setInt("texture2", 1);
 
   // render loop
   // -----------
@@ -140,14 +163,16 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // draw our first triangle
-    shader.use();
+    // bind textures on corresponding texture units
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
 
-    // 第一个参数是我们打算绘制的OpenGL图元的类型
-    // 第二个参数指定了顶点数组的起始索引，我们这里填0
-    // 最后一个参数指定我们打算绘制多少个顶点
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glBindVertexArray(0); // no need to unbind it every time
+    // render container
+    shader.use();
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
     // -------------------------------------------------------------------------------
