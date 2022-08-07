07b4ffcadb966658ca0b0a08ee96d7f1e7b81ddf
navono <navono007@gmail.com>
2022-08-07 14:40:47 +0800

    chore: 升级版本

--- include/logger.h
+++ include/logger.h
@@ -8,15 +8,13 @@
 static quill::Logger* initLogger(const std::string& logFilename = "app.log", const std::string& name = "app") {
   quill::start(false, {});
 
-  auto formatter = QUILL_STRING(
-      "[%(ascii_time)] #%(process).%(thread) %(filename)/%(function_name:):%(lineno:<8) [%(logger_name)] "
-      "%(level_name) %(message)");
-
   auto file_handler = quill::rotating_file_handler(logFilename, "a", 1024 * 1024 * 5, 5);
-  file_handler->set_pattern(formatter, "%D %H:%M:%S.%Qms %z", quill::Timezone::LocalTime);
+  file_handler->set_pattern("[%(ascii_time)] #%(process).%(thread) %(filename)/%(function_name:):%(lineno:<8) [%(logger_name)] "
+      "%(level_name) %(message)", "%D %H:%M:%S.%Qms %z", quill::Timezone::LocalTime);
 
   auto console_handler = quill::stdout_handler();
-  console_handler->set_pattern(formatter, "%D %H:%M:%S.%Qms %z", quill::Timezone::LocalTime);
+  console_handler->set_pattern("[%(ascii_time)] #%(process).%(thread) %(filename)/%(function_name:):%(lineno:<8) [%(logger_name)] "
+      "%(level_name) %(message)", "%D %H:%M:%S.%Qms %z", quill::Timezone::LocalTime);
 
   auto logger = quill::create_logger(name.c_str(), {file_handler, console_handler});
   logger->set_log_level(quill::LogLevel::TraceL3);
