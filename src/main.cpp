9c77a29e25793b66e50090ac6505858fad06ac59
navono <navono007@gmail.com>
2022-08-09 16:21:31 +0800

    style: format

--- src/main.cpp
+++ src/main.cpp
@@ -201,7 +201,9 @@ int main() {
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
